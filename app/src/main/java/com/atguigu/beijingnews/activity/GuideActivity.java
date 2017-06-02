6a1ea5fe77db47baf6faaa965d6e953211931414
lianshuideyu <834893479@qq.com>
2017-06-02 14:54:44 +0800

    引导页面下方的小红点随页面移动而移动

--- app/src/main/java/com/atguigu/beijingnews/activity/GuideActivity.java
+++ app/src/main/java/com/atguigu/beijingnews/activity/GuideActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -30,9 +32,13 @@ public class GuideActivity extends AppCompatActivity {
     LinearLayout llPointGroup;
     @InjectView(R.id.activity_guide)
     RelativeLayout activityGuide;
+    @InjectView(R.id.iv_red_point)
+    ImageView ivRedPoint;
 
     private ArrayList<ImageView> imageViews;
-    private int[] ids = {R.drawable.guide_1,R.drawable.guide_2,R.drawable.guide_3};
+    private int[] ids = {R.drawable.guide_1, R.drawable.guide_2, R.drawable.guide_3};
+    //两点间的距离
+    private int leftMargin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,11 +52,24 @@ public class GuideActivity extends AppCompatActivity {
         vp.setAdapter(new MyPagerAdapter());
 
         vp.addOnPageChangeListener(new MyOnPageChangeListener());
+
+        //计算两个点的间距
+        ivRedPoint.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                //首先先取消之前的---取消监听（提高效率）
+                ivRedPoint.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                // 间距 = 第1个点距离左边的距离 - 第0个点距离左边的距离
+                leftMargin = llPointGroup.getChildAt(1).getLeft() - llPointGroup.getChildAt(0).getLeft();
+                Log.e("TAG", "leftMargin==" + leftMargin);
+            }
+        });
+
     }
 
     private void initData() {
         imageViews = new ArrayList<>();
-        for(int i = 0; i < ids.length; i++){
+        for (int i = 0; i < ids.length; i++) {
             ImageView imageView = new ImageView(this);
             //设置背景
             imageView.setBackgroundResource(ids[i]);
@@ -59,11 +78,11 @@ public class GuideActivity extends AppCompatActivity {
             //添加三个灰点
             ImageView point = new ImageView(this);
             point.setImageResource(R.drawable.guide_point_normal);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(DensityUtil.dip2px(GuideActivity.this,10),DensityUtil.dip2px(GuideActivity.this,10));
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(DensityUtil.dip2px(GuideActivity.this, 10), DensityUtil.dip2px(GuideActivity.this, 10));
             point.setLayoutParams(params);
-            
-            if(i != 0) {
-                params.leftMargin = DensityUtil.dip2px(GuideActivity.this,10);
+
+            if (i != 0) {
+                params.leftMargin = DensityUtil.dip2px(GuideActivity.this, 10);
             }
             //添加到线性布局
             llPointGroup.addView(point);
@@ -84,6 +103,7 @@ public class GuideActivity extends AppCompatActivity {
 
         /**
          * 判断当前View和instantiateItem返回的是否是同一个页面
+         *
          * @param view
          * @param object instantiateItem方法返回的
          * @return
@@ -95,8 +115,9 @@ public class GuideActivity extends AppCompatActivity {
 
         /**
          * 相当于ListView适配器中的getView方法，创建item布局，要把当前视图添加到容器中
+         *
          * @param container ViewPager自身
-         * @param position 要添加或者要创建的位置
+         * @param position  要添加或者要创建的位置
          * @return
          */
         @Override
@@ -110,9 +131,10 @@ public class GuideActivity extends AppCompatActivity {
 
         /**
          * 销毁视图
+         *
          * @param container Viewpager
-         * @param position 要销毁的位置
-         * @param object 要销毁的页面
+         * @param position  要销毁的位置
+         * @param object    要销毁的页面
          */
         @Override
         public void destroyItem(ViewGroup container, int position, Object object) {
@@ -124,24 +146,34 @@ public class GuideActivity extends AppCompatActivity {
     private class MyOnPageChangeListener implements ViewPager.OnPageChangeListener {
         /**
          * 当滑到的时候回调
-         * @param position  当前滑动的页面的下标位置
-         * @param positionOffset 滑动百分比
+         *
+         * @param position             当前滑动的页面的下标位置
+         * @param positionOffset       滑动百分比
          * @param positionOffsetPixels 滑动的像素
          */
         @Override
         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            //红点移动的距离： 间距 = ViewPager滑动的百分比
+            //红点移动的距离 = 间距*ViewPager滑动的百分比
+            float left = leftMargin *(positionOffset + position) ;
+
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) ivRedPoint.getLayoutParams();;
+            params.leftMargin = (int) left;
+            ivRedPoint.setLayoutParams(params);
+            Log.e("TAG","position=="+position+",positionOffset=="+positionOffset+",positionOffsetPixels=="+positionOffsetPixels);
 
         }
 
         /**
          * 当选中某个页面的时候回调
+         *
          * @param position
          */
         @Override
         public void onPageSelected(int position) {
-            if(position == imageViews.size() - 1) {
+            if (position == imageViews.size() - 1) {
                 btnStartMain.setVisibility(View.VISIBLE);
-            }else {
+            } else {
                 btnStartMain.setVisibility(View.GONE);
             }
         }
