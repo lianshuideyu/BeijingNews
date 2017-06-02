3e7bbdd05f7c2ca1a57641ec00f2a1763a11480a
lianshuideyu <834893479@qq.com>
2017-06-02 11:15:25 +0800

    Initial commit

---
+++ app/src/main/java/com/atguigu/beijingnews/WelcomeActivity.java
@@ -0,0 +1,65 @@
+package com.atguigu.beijingnews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.widget.RelativeLayout;
+
+import com.atguigu.beijingnews.activity.MainActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private RelativeLayout rl_root;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        rl_root = (RelativeLayout)findViewById(R.id.rl_root);
+
+        //缩放，渐变，旋转
+        ScaleAnimation sa = new ScaleAnimation(0, 1, 0, 1, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        sa.setDuration(2000);
+        sa.setFillAfter(true);
+
+        AlphaAnimation aa = new AlphaAnimation(0, 1);
+        aa.setDuration(2000);
+        aa.setFillAfter(true);
+
+        RotateAnimation ra = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        ra.setDuration(2000);
+        ra.setFillAfter(true);
+
+        AnimationSet set = new AnimationSet(false);
+        set.addAnimation(sa);
+        set.addAnimation(aa);
+        set.addAnimation(ra);
+        rl_root.startAnimation(set);
+
+        set.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+                startActivity(intent);
+                //关闭欢迎页面
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
